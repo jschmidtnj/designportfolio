a8b3426bdce040e28c7343c074cad03c375485f6
Annette <annettevonbrandis@gmail.com>
2019-08-14 16:38:32 -0400

    tile carousel: fixed bug preventing build
    fixed bug with es6 preventing the website from building
    
    Signed-off-by: Annette <annettevonbrandis@gmail.com>

--- nuxt/components/TileCarousel.vue
+++ nuxt/components/TileCarousel.vue
@@ -195,31 +195,30 @@ export default Vue.extend({
      * @param {number} duration -  Duration of scrolling animation in millisec
      */
     scrollTo(element, scrollPixels, duration) {
+      // Get the start timestamp
+      const startTime =
+        "now" in window.performance
+          ? performance.now()
+          : new Date().getTime();
+      const scroll = (timestamp) => {
+        //Calculate the timeelapsed
+        const timeElapsed = timestamp - startTime;
+        //Calculate progress 
+        const progress = Math.min(timeElapsed / duration, 1);
+        //Set the scrolleft
+        element.scrollLeft = scrollPos + scrollPixels * progress;
+        //Check if elapsed time is less then duration then call the requestAnimation, otherwise exit
+        if (timeElapsed < duration) {
+          //Request for animation
+          window.requestAnimationFrame(scroll);
+        } else {
+          return;
+        }
+      }
       const scrollPos = element.scrollLeft;
       // Condition to check if scrolling is required
       if ( !( (scrollPos === 0 || scrollPixels > 0) && (element.clientWidth + scrollPos === element.scrollWidth || scrollPixels < 0))) 
       {
-        // Get the start timestamp
-        const startTime =
-          "now" in window.performance
-            ? performance.now()
-            : new Date().getTime();
-        
-        function scroll(timestamp) {
-          //Calculate the timeelapsed
-          const timeElapsed = timestamp - startTime;
-          //Calculate progress 
-          const progress = Math.min(timeElapsed / duration, 1);
-          //Set the scrolleft
-          element.scrollLeft = scrollPos + scrollPixels * progress;
-          //Check if elapsed time is less then duration then call the requestAnimation, otherwise exit
-          if (timeElapsed < duration) {
-            //Request for animation
-            window.requestAnimationFrame(scroll);
-          } else {
-            return;
-          }
-        }
         //Call requestAnimationFrame on scroll function first time
         window.requestAnimationFrame(scroll);
       }
