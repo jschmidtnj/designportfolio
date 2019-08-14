3394510e650cd6d77358d0dc82bba2ff51083efc
Annette <annettevonbrandis@gmail.com>
2019-08-13 22:22:27 -0400

    [skip ci] styling: added avb specific stuff
    added avb specific navbar and about page (initial)
    
    Signed-off-by: Annette <annettevonbrandis@gmail.com>

--- nuxt/pages/about.vue
+++ nuxt/pages/about.vue
@@ -2,13 +2,8 @@
   <b-container>
     <b-jumbotron header="About me" class="mt-4"></b-jumbotron>
     <b-list-group>
-      <b-list-group-item
-        >Student at Stevens Institute of Technology</b-list-group-item
-      >
-      <b-list-group-item>Ultimate Frisbee Player</b-list-group-item>
-      <b-list-group-item>Computer Engineering Major</b-list-group-item>
-      <b-list-group-item>Electronics guru</b-list-group-item>
-      <b-list-group-item>Experienced Software Developer</b-list-group-item>
+      <b-list-group-item>Graphic Designer</b-list-group-item>
+      <b-list-group-item>UX Expert</b-list-group-item>
     </b-list-group>
   </b-container>
 </template>
